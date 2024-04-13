39956580065351e76815db777718863cf4451b7c
onick ahmed <onickahmed@gmail.com>
2024-04-13 03:11:58 +0100

    project done

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,56 +2,67 @@ let seats = [];
 let seatLeft = document.getElementById("seatLeft");
 let total = document.getElementById("totalPrice");
 let grandTotal = document.getElementById("grandTotal");
+const discountArea = document.getElementById("discountArea");
+let couponButton = document.getElementById("couponButton");
+let confirmButton = document.getElementById("confirmTicket");
+
+window.onload = confirmButton.disabled = true;
+window.onload = couponButton.disabled = true;
+
 function sortSeat(elementId) {
   const element = document.getElementById(elementId);
   const isActive = element.classList.contains("bg-[#1DD100]");
-
-  console.log(seats.length);
   if (isActive) {
-    element.classList.add("bg-[#F7F8F8]");
-    element.classList.remove("bg-[#1DD100]");
+    leftSeatBg(element);
     removeSeat(element, seats);
+    hideDiscountArea();
+    couponButtonToggle(seats);
+    couponSection.classList.remove("hidden");
   } else {
     if (seats.length + 1 <= 4) {
-      element.classList.remove("bg-[#F7F8F8]");
-      element.classList.add("bg-[#1DD100]");
+      bookedSeatBg(element);
       seats.push(element);
       addSeat(element, seats);
+      couponButtonToggle(seats);
     } else {
       maxSeatModal.showModal();
     }
   }
-
+  ticketReady();
   seatLeft.innerText = 40 - seats.length;
-  total.innerText = seats.length * 500;
-  grandTotal.innerText = seats.length * 500;
+  total.innerText = seats.length * 550;
+  grandTotal.innerText = seats.length * 550;
 }
 
 function applyCoupon() {
   const coupon = document.getElementById("couponCode").value;
   const coupon15 = document.getElementById("coupon15").innerText;
   const coupon20 = document.getElementById("coupon20").innerText;
-  const grandTotal = document.getElementById("grandTotal");
+  const discountPrice = document.getElementById("discountPrice");
 
   if (coupon == coupon15 && total.innerText >= 2000) {
-    grandTotal.innerText = total.innerText - (total.innerText * 15) / 100;
+    showDiscountPrice(15);
   } else if (coupon == coupon20 && total.innerText >= 2000) {
-    grandTotal.innerText = total.innerText - (total.innerText * 20) / 100;
+    showDiscountPrice(20);
   } else {
-    grandTotal.innerText = seats.length * 500;
+    grandTotal.innerText = seats.length * 550;
+    discountPrice.innerText = "00";
+    wrongCouponModal.showModal();
+  }
+}
+
+function ticketReady() {
+  if (checkTicketValidity(seats)) {
+    confirmButton.disabled = false;
+    return true;
+  } else {
+    confirmButton.disabled = true;
+    return false;
   }
 }
 
 function ticketDone() {
-  const name = document.getElementById("customerName").value;
-  const phone = document.getElementById("customerPhone").value;
-  const successModal = document.getElementById("successModal");
-
-  if (name.length >= 2 && phone.length == 11 && seats.length > 0) {
+  if (ticketReady()) {
     successModal.showModal();
-  } else {
-    errorModal.showModal();
   }
 }
-
-// bg-[url('/assets/images/banner.png')] bg-no-repeat bg-cover
--- assets/js/utility.js
+++ assets/js/utility.js
@@ -1,4 +1,5 @@
 let seatCount = document.getElementById("seatCount");
+const discountPrice = document.getElementById("discountPrice");
 
 function addSeat(selectedSeat, totalSeat) {
   // Get the table element in which you want to add row
@@ -17,7 +18,6 @@ function addSeat(selectedSeat, totalSeat) {
   let c2 = row.insertCell(1);
   let c3 = row.insertCell(2);
 
-  // Add data to c1 and c2
   c1.innerText = selectedSeat.id;
   c2.innerText = "Economy";
   c3.innerText = "500";
@@ -26,16 +26,60 @@ function addSeat(selectedSeat, totalSeat) {
 }
 
 function removeSeat(selectedSeat, seatArray) {
-  let total = document.getElementById("totalPrice");
-  // removing from the selected seat table
   let row = document.getElementById("seat" + selectedSeat.id);
   row.parentNode.removeChild(row);
-
-  //   removing from the array
   seatArray.splice(
     seatArray.findIndex((a) => a.id === selectedSeat.id),
     1
   );
   let numberOfSeat = seatArray.length;
   seatCount.innerText = numberOfSeat;
+}
+
+function checkTicketValidity() {
+  const name = document.getElementById("customerName").value;
+  const phone = document.getElementById("customerPhone").value;
+  if (name.length >= 2 && phone.length > 0 && seats.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function couponButtonToggle(seats) {
+  if (seats.length == 4) {
+    couponButton.disabled = false;
+  } else {
+    couponButton.disabled = true;
+  }
+}
+
+function showDiscountArea() {
+  discountArea.classList.remove("hidden");
+  discountArea.classList.add("flex");
+}
+
+function showDiscountPrice(percentage) {
+  const couponSection = document.getElementById("couponSection");
+  grandTotal.innerText = total.innerText - (total.innerText * percentage) / 100;
+  discountPrice.innerText = (total.innerText * percentage) / 100;
+  couponSection.classList.add("hidden");
+  showDiscountArea();
+}
+
+function hideDiscountArea() {
+  discountArea.classList.remove("flex");
+  discountArea.classList.add("hidden");
+}
+
+function leftSeatBg(element) {
+  console.log("left");
+  element.classList.add("bg-[#F7F8F8]");
+  element.classList.remove("bg-[#1DD100]");
+}
+
+function bookedSeatBg(element) {
+  console.log("booked");
+  element.classList.remove("bg-[#F7F8F8]");
+  element.classList.add("bg-[#1DD100]");
 }
